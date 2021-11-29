14eb2f898d0fb15f5a19c60724dee6bc8058d4a6
Soliquidus <tibo.pfeifer@gmail.com>
2021-11-29 16:43:06 +0100

    Delete function et CLI command added

--- main.go
+++ main.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"dictionary/dictionary"
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
+	action := flag.String("action", "list", "Action to perform on the dictionary")
+
 	d, err := dictionary.New("./badger")
 	handleErr(err)
 	defer d.Close()
 
-	err1 := d.Add("python", "An interpreted language")
-	if err1 != nil {
-		return
+	flag.Parse()
+	switch *action {
+	case "list":
+		actionList(d)
+	default:
+		fmt.Printf("Unknown action: %v\n", *action)
 	}
-	words, entries, _ := d.List()
-	for _, words := range words {
-		fmt.Println(entries[words])
-	}
+}
 
+func actionList(d *dictionary.Dictionary) {
+	words, entries, err := d.List()
+	handleErr(err)
+	fmt.Println("Dictionary content")
+	for _, word := range words {
+		fmt.Println(entries[word])
+	}
 }
 
 func handleErr(err error) {
